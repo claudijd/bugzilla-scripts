756ce08df18c5e5760d590c3a961ed2dc575faf2
Jonathan Claudius <jclaudius@mozilla.com>
2019-01-09 15:39:14 -0500

    Add more historic refs

--- security_assessment_products/generate_assessment_metrics.py
+++ security_assessment_products/generate_assessment_metrics.py
@@ -2,9 +2,14 @@
 import bugsy
 import getpass
 import json
+from datetime import datetime, timedelta
 
 api_key = getpass.getpass("Enter Bugzilla API Key: ")
 bugzilla = bugsy.Bugsy(api_key=api_key)
+
+today = str(datetime.today()).split(" ")[0]
+month_ago = str(datetime.today() - timedelta(days=30)).split(" ")[0]
+two_month_ago = str(datetime.today() - timedelta(days=60)).split(" ")[0]
 
 # Get Vulnerability Assessment Metrics Data
 va_bugs = bugzilla.search_for\
@@ -92,7 +97,7 @@ print("# In the last 30 days...")
 va_bugs_30 = bugzilla.search_for\
     .product('Enterprise Information Security')\
     .component('Vulnerability Assessment')\
-    .timeframe('2018-12-09', '2019-01-09')\
+    .timeframe(month_ago, today)\
     .search()
 
 va_metrics_30 = []
@@ -112,7 +117,7 @@ for bug in va_bugs_30:
 pt_bugs_30 = bugzilla.search_for\
     .product('Enterprise Information Security')\
     .component('Penetration Test')\
-    .timeframe('2018-12-09', '2019-01-09')\
+    .timeframe(month_ago, today)\
     .search()
 
 pt_metrics_30 = []
@@ -162,6 +167,84 @@ for bug in va_metrics_30:
         inprogress_vas_30 += 1
 
 print("- {} vulnerability assessments are in progress.".format(inprogress_vas_30))
+
+print("")
+print("# In the prior 30 day period...")
+
+# Get Vulnerability Assessment Metrics Data
+va_bugs_60 = bugzilla.search_for\
+    .product('Enterprise Information Security')\
+    .component('Vulnerability Assessment')\
+    .timeframe(two_month_ago, month_ago)\
+    .search()
+
+va_metrics_60 = []
+
+for bug in va_bugs_60:
+    raw_whiteboard = bug._bug['whiteboard']
+
+    if raw_whiteboard:
+        whiteboard = dict(x.split('=') for x in raw_whiteboard.split(' '))
+    else:
+        whiteboard = ""
+
+    va_metrics_60.append(
+        {'id': bug.id, 'summary': bug.summary, 'status': bug.status, 'resolution': bug.resolution, 'whiteboard': whiteboard})
+
+# Get Pentest Assessment Metrics Data
+pt_bugs_60 = bugzilla.search_for\
+    .product('Enterprise Information Security')\
+    .component('Penetration Test')\
+    .timeframe(two_month_ago, month_ago)\
+    .search()
+
+pt_metrics_60 = []
+
+for bug in pt_bugs_60:
+    raw_whiteboard = bug._bug['whiteboard']
+
+    if raw_whiteboard:
+        whiteboard = dict(x.split('=') for x in raw_whiteboard.split(' '))
+    else:
+        whiteboard = ""
+
+    pt_metrics_60.append(
+        {'id': bug.id, 'summary': bug.summary, 'status': bug.status, 'resolution': bug.resolution, 'whiteboard': whiteboard})
+
+assessment_metrics_60 = {
+    'vulnerability assessment': va_metrics_60,
+    'penetration assessment': pt_metrics_60
+}
+
+# How many vulnerability assessments have we done?
+print("- {} vulnerability assessments were requested.".format(len(va_metrics_60)))
+
+# How many vulnerability assessment have we completed?
+completed_vas_60 = 0
+
+for bug in va_metrics_60:
+    if bug['status'] == "RESOLVED" and not bug['resolution'] == "INVALID":
+        completed_vas_60 += 1
+
+print("- {} vulnerability assessments were completed.".format(completed_vas_60))
+
+# How many vulnerability assessments are invalid?
+invalid_vas_60 = 0
+
+for bug in va_metrics_60:
+    if bug['status'] == "RESOLVED" and bug['resolution'] == "INVALID":
+        invalid_vas_60 += 1
+
+print("- {} vulnerability assessments were invalid.".format(invalid_vas_60))
+
+# How many vulnerability assessments have are in progress?
+inprogress_vas_60 = 0
+
+for bug in va_metrics_60:
+    if bug['status'] == "NEW" or bug['status'] == "ASSIGNED":
+        inprogress_vas_60 += 1
+
+print("- {} vulnerability assessments are in progress.".format(inprogress_vas_60))
 
 
 # ADD NEW THING WE WANT TO LEARN ABOUT FROM OUR METRICS
